a8ddee04a355eebe21ebf288f515be66df1ceceb
afii-sf <contact.afii.sf@gmail.com>
2024-08-27 18:57:21 +0200

    Update Nav.jsx

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
         <nav className="navbar navbar-expand-lg">
           <div className="container">
             {/* logo  */}
-            <Link className="navbar-brand fs-6" to={"/"} style={{color:"#C56A4B"}}><img src="https://s3-alpha-sig.figma.com/img/e76d/7a48/2f82f69a077e4d874a9a3ee021263566?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=a~lgdB3ejQvpTLB3d3-7DT4BooQEpcEr8VvM13hGxwXM2ISeb5X77uv8c-KBSgfMQXpRqSYXB1uMwmTVos4igrORmJ7U6eQD3DIXnRsuuvOVb5wcXHHzyrkxzgLh5MlmCJlbUhaHpj11rLm3H7xcY2MTEbHaxYvQDqOBeksy~fR0vh3n0MV9~B50mGI4JhhfXqQyheYAitdPhugOlC9JMTsLVfJSsoKeFzOthS35iCzTYgPPBppYNvHqUY0w4rZzA-HkqqRk42Kv3zRMiQt-nNpBCAcSqzf0GL7nRZJDjfWJjzFxhF3nuYsYVaM3L70ug5k-4E9yA9r9Y57XkzHQGQ__" alt="AFII" width="30" height="24"></img>afii.us</Link>
+            <Link className="navbar-brand fs-6" to={"/"} style={{color:"#C56A4B"}}><img src="/static/images/afii_logo.png" alt="AFII" width="30" height="24"></img>afii.us</Link>
             {/* toggle button */}
             <button className="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
